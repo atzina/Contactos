d846fa81ead8a4fbfb9fef931f0810037befea9b
atzina <atzina@gmail.com>
2023-09-12 21:34:48 -0600

    fondo y mock añadido

--- src/Components/MockApi.jsx
+++ src/Components/MockApi.jsx
@@ -2,8 +2,8 @@
 const contacts = [
     {id:1, name: 'Juana Sanchez', email: 'juana@email.com'},
     {id:2, name: 'Oscar Chavez', email: 'oscar@email.com'},
-    {id:3, name: 'Sury Lee', email:'suriz@email.com'},
-    {id:4, name: 'Atzin', email:'atzin@mail.com'},
+    {id:3, name: 'Sury Lee Cortés', email:'suriz@email.com'},
+    {id:4, name: 'Lluvia Solares', email:'lluv@mail.com'},
     {id:5, name: 'Servicios varios', email:'servicios@gmail.com'},
 ]
 
