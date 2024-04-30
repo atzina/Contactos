9832348d458e456d15944e244ec9e436f632fd8a
atzina <atzina@gmail.com>
2024-04-30 13:33:30 -0600

    1er intento de agregar campo

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -6,7 +6,7 @@ function ContactForm({ onSubmit, onUpdate, contactToEdit }) {
     <>
         <h3>{contactToEdit ? 'Editar contacto' : 'Inserta un contacto nuevo'}</h3>
       <Formik
-        initialValues={{ name: contactToEdit?.name || '', email: contactToEdit?.email || '' }}
+        initialValues={{ name: contactToEdit?.name || '', email: contactToEdit?.email || '', num: contactToEdit?.num || '' }}
         onSubmit={(values, { resetForm }) => {
           if (contactToEdit) {
             // Si hay un contacto para editar, actualiza en lugar de agregar uno nuevo
@@ -21,6 +21,7 @@ function ContactForm({ onSubmit, onUpdate, contactToEdit }) {
         <Form>
           <Field type="text" name="name" placeholder="Nombre" />
           <Field type="email" name="email" placeholder="Correo electrónico" />
+          <Field type="tel" name="telefono" placeholder= "Teléfono"/>
           <button type="submit">{contactToEdit ? 'Actualizar' : 'Guardar'}</button>
         </Form>
       </Formik>
--- src/Components/MockApi.jsx
+++ src/Components/MockApi.jsx
@@ -1,10 +1,10 @@
 
 const contacts = [
-    {id:1, name: 'Juana Sanchez', email: 'juana@email.com'},
-    {id:2, name: 'Oscar Chavez', email: 'oscar@email.com'},
-    {id:3, name: 'Sury Lee Cortés', email:'suriz@email.com'},
-    {id:4, name: 'Lluvia Solares', email:'lluv@mail.com'},
-    {id:5, name: 'Servicios varios', email:'servicios@gmail.com'},
+    {id:1, name: 'Juana Sanchez', email: 'juana@email.com', num: '55 54 53 52 51'},
+    {id:2, name: 'Oscar Chavez', email: 'oscar@email.com', num: '55 18 20 52 15'},
+    {id:3, name: 'Sury Lee Cortés', email:'suriz@email.com', num: '55 23 53 30 11'},
+    {id:4, name: 'Lluvia Solares', email:'lluv@mail.com', num: '55 54 53 52 51'},
+    {id:5, name: 'Servicios varios', email:'servicios@gmail.com', num: '55 80 25 44 78'},
 ]
 
 export default contacts;
